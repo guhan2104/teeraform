3b4b06de83d929330d8cb01b5066935552339631
Guhiiii <guhanshivaji21@gmail.com>
2023-11-21 09:17:55 +0530

    change

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = var.region_name
+  region  = "ap-south-1"
   profile = "default1"
 }
 resource "aws_instance" "web" {
@@ -8,27 +8,46 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = ["sg-04e782c9493b7bceb"] 
   #security_groups = "sg-04e782c9493b7bceb"
   #first method
-  key_name = "TF_key"
-
-
-  tags = {
-    Name = "Terraform Ec2"
-  }
+  key_name = aws_key_pair.key_pair.key_name
 }
 
-#keypair second method for Key_pair
+#   tags = {
+#     Name = "Terraform Ec2"
+#   }
+# }
 
-resource "aws_key_pair" "TF_key" {
-  key_name   = "TF_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
+# resource "aws_key_pair" "TF_key" {
+#   key_name   = "TF_key"
+#   public_key = tls_private_key.rsa.public_key_openssh
+# }
+
+# resource "tls_private_key" "rsa" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+
+# resource "local_file" "TF-key" {
+#     content  = tls_private_key.rsa.private_key_pem
+#     filename = "TF_key.pem"
+# }
+
 
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "TF-key" {
+resource "local_file" "private_key" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "tfkey.pem"
+  filename = "private_key.pem"
 }
+resource "local_file" "public_key_ppk" {
+  content  = tls_private_key.rsa.public_key_openssh
+  filename = "public_key.pub"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "tf_key_pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
